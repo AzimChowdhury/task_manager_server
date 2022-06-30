b603f803c18678598a9c0c7a31cbf6f9eabf6485
Azim Chowdhury <azimchy994@gmail.com>
2022-07-01 01:58:51 +0600

    d

--- index.js
+++ index.js
@@ -1,20 +1,82 @@
 const express = require('express');
-const app=express();
+const app = express();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const mongodb = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+function run() {
+    const uri = `mongodb+srv://task-manager:${process.env.PASSWORD}@cluster0.c2swy.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.get('/', (req,res)=>{
-    res.send('server running')
-})
 
-app.listen(port,()=>{
-    console.log('server running on the port', port)
-})
+    try {
+        client.connect()
+        const taskCollection = client.db('task-manager').collection('taskCollection');
+        const completedCollection = client.db('task-manager').collection('completedCollection');
+
+
+
+        //post a task
+        app.post('/addTask', async (req, res) => {
+            const task = req.body;
+            const result = await taskCollection.insertOne(task);
+            res.send(result)
+        });
+
+        //get my tasks by email
+        app.get('/myTasks/:email', async (req, res) => {
+            const email = req.params.email;
+            const cursor = { email: email };
+            const result = await taskCollection.find(cursor).toArray();
+            res.send(result)
+        })
+
+        //add task to completed
+        app.post('/completed',async (req, res) => {
+            const task = req.body;
+            const result =await completedCollection.insertOne(task)
+            res.send(result)
+        })
+
+        //delete completed task
+        app.delete('/deleteTask/:id',async(req,res)=>{
+            const id= req.params.id;
+            const query = {_id:ObjectId(id)}
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+        })
+
+    }
+
+
+    finally {
+        // perform actions on the collection object
+        //   client.close();
+    }
+
+    app.get('/', (req, res) => {
+        res.send('server running')
+    })
+
+    app.listen(port, () => {
+        console.log('server running on the port', port)
+    })
+}
+
+run()
+
+
+
+
+
+
+
+
+
+
